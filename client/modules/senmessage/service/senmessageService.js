f46dadbfa81e1aacbf1935653e78efe0b4c175c0
seiger <15594818350@163.com>
2017-12-19 19:08:09 +0800

    tmp

--- client/modules/senmessage/service/senmessageService.js
+++ client/modules/senmessage/service/senmessageService.js
@@ -3,8 +3,8 @@ angular.module("senmessageService", ["commons"])
     .factory("SenFac", ['$resource', 'parseResource', function ($resource, parseResource) {
         var factories = {};
 
-        // factories.getSenLists = $resource(CQ.variable.RESTFUL_URL + "senmassage/showmsg", parseResource.params, parseResource.actions);
-        factories.getSenLists = $resource("http://118.190.133.203:8100/yqdata/senmassage/showmsg", parseResource.params, parseResource.actions);
+        factories.getSenLists = $resource(CQ.variable.RESTFUL_URL + "senmassage/showmsg", parseResource.params, parseResource.actions);
+        // factories.getSenLists = $resource("http://118.190.133.203:8100/yqdata/senmassage/showmsg", parseResource.params, parseResource.actions);
 
         factories.getDetailData = $resource(CQ.variable.RESTFUL_URL + "senmassage/showrawmsg", parseResource.params, parseResource.actions);
 
