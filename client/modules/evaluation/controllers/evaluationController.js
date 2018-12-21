23fe313b5fd7585d39ad27c7815d7f5a5840abb2
Skeanmy <396966928@qq.com>
2018-12-21 16:50:28 +0800

    api规范

--- client/modules/evaluation/controllers/evaluationController.js
+++ client/modules/evaluation/controllers/evaluationController.js
@@ -580,29 +580,45 @@ CQ.mainApp.dkController
                 xAxis: {
                     type: 'category',
                     boundaryGap: false,
-                    data: date
+                    data: ['2017/08/17','2017/08/19','2017/08/21','2017/08/23'],
+                    axisLabel: {        
+                                show: true,
+                                fontSize:30,
+                                textStyle: {
+                                    color: '#fff',
+                                    
+                                }
+                            }
                 },
                 yAxis: {
                     type: 'value',
-                    boundaryGap: [0, '100%']
+                    boundaryGap: [0, '100%'],
+                    axisLabel: {    
+                                fontSize:30,    
+                                show: true,
+                                textStyle: {
+                                    color: '#fff',
+                                    fontSize:'25'
+                                }
+                            }
                 },
-                dataZoom: [{
-                    type: 'inside',
-                    start: 20,
-                    end: 100
-                }, {
-                    start: 0,
-                    end: 10,
-                    handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
-                    handleSize: '80%',
-                    handleStyle: {
-                        color: '#fff',
-                        shadowBlur: 3,
-                        shadowColor: 'rgba(0, 0, 0, 0.6)',
-                        shadowOffsetX: 2,
-                        shadowOffsetY: 2
-                    }
-                }],
+                // dataZoom: [{
+                //     type: 'inside',
+                //     start: 20,
+                //     end: 100
+                // }, {
+                //     start: 0,
+                //     end: 10,
+                //     handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+                //     handleSize: '80%',
+                //     handleStyle: {
+                //         color: '#fff',
+                //         shadowBlur: 3,
+                //         shadowColor: 'rgba(0, 0, 0, 0.6)',
+                //         shadowOffsetX: 2,
+                //         shadowOffsetY: 2
+                //     }
+                // }],
                 series: [
                 {
                     name: '当前舆情态势值',
@@ -613,10 +629,19 @@ CQ.mainApp.dkController
                     sampling: 'average',
                     itemStyle: {
                         normal: {
-                            color: 'rgb(255, 70, 131)'
+                            color: 'rgb(255, 70, 131)',
+                            label: {
+                          show: true,
+                          position: 'top',
+                          textStyle: {
+                            color: 'black',
+                            fontSize:35
+                      }
+
+                  }
                         }
                     },
-                    data: taishi
+                    data: [44,58,46,41]
                 },
                 {
                     name:'安全态势',
@@ -642,7 +667,7 @@ CQ.mainApp.dkController
                             }])
                         }
                     },
-                    data:safe
+                    data:[60,60,60,60]
                 },
                 {
                     name:'爆发',
@@ -668,7 +693,7 @@ CQ.mainApp.dkController
                             }])
                         }
                     },
-                    data: dangerous
+                    data: [50,50,50,50]
                 }]
             };
             mychart.setOption(option);
